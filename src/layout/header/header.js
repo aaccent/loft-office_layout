2ae3b775a88edb1cc97010fe614fea6a74a10afb
Sevastyan <seva@aaccent.ru>
2025-02-21 15:28:44 +0300

    feat: handler for opening/closing catalog-menu

--- src/layout/header/header.js
+++ src/layout/header/header.js
@@ -1,12 +1,56 @@
-const header = document.querySelector('.header')
+/** Высота и позиционирование шапки и его меню */
+void (function () {
+    const header = document.querySelector('.header')
 
-function setHeaderHeight() {
-    if (!header) return
+    /** Выставляет переменную в CSS с высотой шапки для позиционирования меню */
+    function setHeaderHeight() {
+        if (!header) return
 
-    const height = `${header.offsetHeight}px`
-    header.style.marginBottom = `-${height}`
-    document.documentElement.style.setProperty('--header-height', height)
-}
-setHeaderHeight()
+        const height = `${header.offsetHeight}px`
+        header.style.marginBottom = `-${height}`
+        document.documentElement.style.setProperty('--header-height', height)
+    }
+    setHeaderHeight()
 
-window.addEventListener('resize', setHeaderHeight)
+    window.addEventListener('resize', setHeaderHeight)
+})()
+
+/** Открытие и закрытие меню каталога */
+void (function () {
+    const catalogMenu = document.querySelector('.catalog-menu')
+
+    /** @type {number | null} */
+    let timeout = null
+    const THRESHOLD_MS_BEFORE_CLOSE = 600
+
+    function clearCatalogTimeout() {
+        if (!timeout) return
+
+        clearTimeout(timeout)
+        timeout = null
+    }
+
+    function openCatalogMenu() {
+        clearCatalogTimeout()
+        if (catalogMenu?.classList.contains('opened')) return
+
+        catalogMenu?.classList.add('opened')
+    }
+
+    /** @param {MouseEvent} event */
+    function deferCloseCatalogMenu(event) {
+        if (!catalogMenu?.classList.contains('opened')) return
+
+        timeout = setTimeout(() => {
+            catalogMenu?.classList.remove('opened')
+        }, THRESHOLD_MS_BEFORE_CLOSE)
+    }
+
+    document.querySelectorAll('[data-action="catalog-menu"]').forEach((button) => {
+        button.addEventListener('mouseenter', openCatalogMenu)
+        button.addEventListener('mouseleave', deferCloseCatalogMenu)
+    })
+
+    catalogMenu.addEventListener('mouseenter', openCatalogMenu)
+    catalogMenu.addEventListener('mouseleave', deferCloseCatalogMenu)
+})()
