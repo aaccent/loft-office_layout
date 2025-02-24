13e45baa94916bc90d72b62e036968b0d1a9af3b
Sevastyan <seva@aaccent.ru>
2025-02-24 13:58:29 +0300

    feat: mobile version of catalog menu

--- src/layout/header/header.js
+++ src/layout/header/header.js
@@ -1,4 +1,6 @@
 /** Высота и позиционирование шапки и его меню */
+import { isDesktop, isMobile } from 'globals/adaptive'
+
 void (function () {
     const header = document.querySelector('.header')
 
@@ -18,6 +20,7 @@ void (function () {
 /** Открытие и закрытие меню каталога */
 void (function () {
     const catalogMenu = document.querySelector('.catalog-menu')
+    if (!catalogMenu) return
 
     /** @type {number | null} */
     let timeout = null
@@ -31,14 +34,16 @@ void (function () {
     }
 
     function openCatalogMenu() {
+        if (isMobile) return
+
         clearCatalogTimeout()
         if (catalogMenu?.classList.contains('opened')) return
 
         catalogMenu?.classList.add('opened')
     }
 
-    /** @param {MouseEvent} event */
-    function deferCloseCatalogMenu(event) {
+    function deferCloseCatalogMenu() {
+        if (isMobile) return
         if (!catalogMenu?.classList.contains('opened')) return
 
         timeout = setTimeout(() => {
@@ -49,8 +54,47 @@ void (function () {
     document.querySelectorAll('[data-action="catalog-menu"]').forEach((button) => {
         button.addEventListener('mouseenter', openCatalogMenu)
         button.addEventListener('mouseleave', deferCloseCatalogMenu)
+        button.addEventListener('click', () => {
+            if (isDesktop) return
+
+            catalogMenu?.classList.toggle('opened')
+        })
     })
 
     catalogMenu.addEventListener('mouseenter', openCatalogMenu)
     catalogMenu.addEventListener('mouseleave', deferCloseCatalogMenu)
+
+    // Открытие и закрытие подпунктов каталога на мобилке
+    document.querySelectorAll('.catalog-menu__list li:has(ul)').forEach((item) => {
+        const link = item.querySelector(':is(a,span,button)')
+        const subcategory = item.querySelector('ul')
+
+        if (!link || !subcategory) return
+
+        link.addEventListener('click', (event) => {
+            if (isDesktop) return
+            event.preventDefault()
+
+            item.classList.toggle('opened')
+        })
+    })
+
+    // Добавление кнопок закрытия подпунктов в мобильной версии
+    document.querySelectorAll('.catalog-menu__list > li').forEach((item) => {
+        const link = item.querySelector(':is(a,span,button)')
+        const ul = item.querySelector('ul')
+        if (!ul) return
+
+        const li = document.createElement('li')
+        li.className = 'catalog-menu__list-item-close-button'
+
+        const closeButton = document.createElement('button')
+        closeButton.innerText = link.textContent
+        closeButton.addEventListener('click', () => {
+            item.classList.remove('opened')
+        })
+
+        li.append(closeButton)
+        ul.prepend(li)
+    })
 })()
