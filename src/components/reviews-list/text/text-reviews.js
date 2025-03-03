68536d5bb061d3f9a2a91d2695297bfef14117ec
Regina <rmyachina@mail.ru>
2025-03-03 14:11:42 +0300

    feat: text review content

--- src/components/reviews-list/text/text-reviews.js
+++ src/components/reviews-list/text/text-reviews.js
@@ -1,10 +1,10 @@
 void (function () {
-    const textReviews = document.querySelectorAll('.text-reviews__item')
+    const textReviews = document.querySelectorAll('.text-reviews-list__item')
 
     textReviews.forEach((review) => {
-        const reviewImages = review.querySelectorAll('.text-reviews__item-text img')
+        const reviewImages = review.querySelectorAll('.text-reviews-list__item-text img')
         if (!reviewImages.length) return
-        const media = review.querySelector('.text-reviews__item-media')
+        const media = review.querySelector('.text-reviews-list__item-media')
         const span = document.createElement('span')
         span.textContent = `${reviewImages.length} фото`
         media.append(span)
