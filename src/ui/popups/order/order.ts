7e939250d7f543b8b8f666059e7fcb4d86ff5968
Regina <rmyachina@mail.ru>
2025-03-05 17:18:22 +0300

    feat: order

--- src/ui/popups/order/order.ts
+++ src/ui/popups/order/order.ts
@@ -1,9 +1,14 @@
 import { disableScroll } from 'features/scroll'
+import { validateTelInput } from 'features/forms'
 
-interface FinalInfo {
+type FinalInfo = {
     name: string
     tel: string
     email: string
+}
+
+interface FinalInfoElement extends HTMLElement {
+    dataset: { input: keyof FinalInfo }
 }
 
 void (function () {
@@ -17,11 +22,25 @@ void (function () {
         currentStep?.nextElementSibling?.classList.add('_opened')
     }
 
-    function setFinalUserInfo(info: FinalInfo) {
-        const userDataStep = document.querySelector<HTMLElement>('.order-step .user-data')
-        const finalInfoElements = userDataStep?.querySelectorAll<HTMLElement>('.user-data__final div')
+    function openCheckedStep(e: MouseEvent) {
+        const changeStepButton = e.target as HTMLElement
+        const step = changeStepButton.closest<HTMLElement>('.order-step')
+        const currentOpenStep = document.querySelector<HTMLElement>('.order-step._opened')
 
-        userDataStep?.classList.add('valid')
+        currentOpenStep?.classList.remove('_opened')
+        step?.classList.add('_opened')
+    }
+
+    function setFinalUserInfo(info: FinalInfo) {
+        const userData = document.querySelector<HTMLElement>('.user-data')
+        const finalInfoElements = document?.querySelectorAll<FinalInfoElement>('.user-data__final div')
+
+        userData?.classList.add('valid')
+
+        finalInfoElements?.forEach((el) => {
+            const key = el.dataset.input
+            el.textContent = info[key]
+        })
     }
 
     const radioInputs = document.querySelectorAll<HTMLInputElement>('input[type="radio"]')
@@ -33,38 +52,49 @@ void (function () {
 
     userDataButton?.addEventListener('click', () => {
         const userDataRequiredInputs = document.querySelectorAll<HTMLInputElement>('.user-data__inputs input')
-        const invalidInputs: HTMLElement[] = []
-
-        const fullName: string[] = []
-
-        // const invalidInputs = userDataRequiredInputs.filter((input) => !input.validity.valid)
-        // if (!invalidInputs.length) {
-        //     openNextStep(userDataButton)
-        //     //setFinalUserInfo()
-        //     return
-        // }
+        const fullName: string[] = ['']
+        const finalInfo: FinalInfo = {
+            name: '',
+            email: '',
+            tel: '',
+        }
 
         userDataRequiredInputs.forEach((input) => {
-            input.addEventListener('input', () => input.classList.remove('invalid'), { once: true })
-            if (!input.validity.valid) {
+            if (!input.validity.valid || !validateTelInput(input)) {
                 input.classList.add('invalid')
-                invalidInputs.push(input)
                 return
             }
+
             switch (input.name) {
                 case 'name':
-                    fullName[1] = input.name
+                    fullName[1] = input.value
                     break
                 case 'last-name':
-                    fullName[0] = input.name
+                    fullName[0] = input.value
                     break
                 case 'middle-name':
-                    fullName[2] = input.name
+                    fullName[2] = input.value
+                    break
+                case 'tel':
+                    finalInfo.tel = input.value
+                    break
+                case 'email':
+                    finalInfo.email = input.value
                     break
             }
         })
+
+        const invalids = document.querySelectorAll('.order-step input.invalid')
+        if (invalids.length) return
+
+        finalInfo.name = fullName.join(' ')
+        openNextStep(userDataButton)
+        setFinalUserInfo(finalInfo)
     })
 
-    const firstStep = document.querySelector<HTMLElement>('.order-step:first-child')
+    const changeStepButtons = document.querySelectorAll<HTMLElement>('.order-step__change-button')
+    changeStepButtons.forEach((button) => button.addEventListener('click', openCheckedStep))
+
+    const firstStep = document.querySelector<HTMLElement>('.order-step:nth-child(1)')
     firstStep?.classList.add('_opened')
 })()
