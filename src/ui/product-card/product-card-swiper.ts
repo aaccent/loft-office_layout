8c1c95a1dea1bad5a45f5408ca536a8017b0ef3c
sevastyan <seva@aaccent.ru>
2025-03-06 16:54:49 +0300

    feat: hover handler for product-card

--- src/ui/product-card/product-card-swiper.ts
+++ src/ui/product-card/product-card-swiper.ts
@@ -1,5 +1,23 @@
 import Swiper from 'swiper'
 import { Pagination, EffectFade } from 'swiper/modules'
+import { adaptive } from 'globals/adaptive'
+
+function initProductCard(cardTop: HTMLElement, slides: HTMLElement[], swiper: Swiper) {
+    const hoverContainer = document.createElement('div')
+    hoverContainer.className = 'product-card__hover-container'
+
+    slides.forEach((_, index) => {
+        const hoverItem = document.createElement('div')
+        hoverItem.className = 'product-card__hover-item'
+        hoverItem.addEventListener('mouseenter', () => {
+            swiper.slideTo(index)
+        })
+
+        hoverContainer.append(hoverItem)
+    })
+
+    cardTop.prepend(hoverContainer)
+}
 
 new Swiper('.product-card .swiper', {
     createElements: true,
@@ -12,4 +30,11 @@ new Swiper('.product-card .swiper', {
         bulletActiveClass: 'slider-pagination-bullet-active',
     },
     modules: [Pagination, EffectFade],
+    on: {
+        init(swiper) {
+            if (adaptive.isDesktop) return
+
+            initProductCard(swiper.el, swiper.slides, swiper)
+        },
+    },
 })
