71b5408d2049907422e40755b5eafd6d539c6b0e
Sevastyan <seva@aaccent.ru>
2025-03-11 18:39:43 +0300

    feat: mobile version of selector

--- src/ui/selector/selector.ts
+++ src/ui/selector/selector.ts
@@ -1,11 +1,18 @@
 document.querySelectorAll('.selector').forEach((selector) => {
     const itemInputs = selector.querySelectorAll<HTMLInputElement>('.selector__item-input')
     const button = selector.querySelector<HTMLElement>('.selector__button')
+    const closeButton = selector.querySelector('.close-btn')
 
     if (!itemInputs.length || !button) return
 
     button.innerText = itemInputs[0].dataset.showValue || ''
     button.addEventListener('click', () => selector.classList.toggle('opened'))
+    closeButton?.addEventListener('click', () => selector.classList.remove('opened'))
+    selector.addEventListener('click', (event) => {
+        if (event.target !== selector) return
+        selector.classList.remove('opened')
+    })
+
     itemInputs[0].checked = true
 
     itemInputs.forEach((input) => {
