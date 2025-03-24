cca57014f78e13f6c77b2b3ad68f637c16e31c31
Sevastyan <seva@aaccent.ru>
2025-03-24 06:22:21 +0300

    refactor: method for styling input fields without using js

--- src/ui/input/input.js
+++ src/ui/input/input.js
@@ -2,14 +2,12 @@ const dataInputs = document.querySelectorAll('.input')
 
 dataInputs.forEach((container) => {
     const input = container.querySelector('input')
-    if (input.value) container.classList.add('process')
 
     const resetButton = document.createElement('button')
     resetButton.type = 'button'
     resetButton.classList.add('input__reset')
     resetButton.addEventListener('click', () => {
         input.value = ''
-        container.classList.remove('process')
     })
     container.append(resetButton)
 
@@ -20,11 +18,9 @@ dataInputs.forEach((container) => {
 
     input.addEventListener('focus', () => {
         input.classList.remove('invalid')
-        container.classList.add('process')
     })
 
     input.addEventListener('blur', () => {
         if (input.value) return
-        container.classList.remove('process')
     })
 })
