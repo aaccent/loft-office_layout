7d4d3d4c7dafd25ee6f3aac5c12984e3d734cb34
Sevastyan <seva@aaccent.ru>
2025-03-24 07:32:43 +0300

    fix: ya map key

--- src/features/createDeliveryMap.ts
+++ src/features/createDeliveryMap.ts
@@ -3,7 +3,7 @@ import { createYMap } from 'features/maps/createYMap'
 void (async function () {
     const mapContainer = document.createElement('div')
     mapContainer.classList.add('delivery-map')
-    mapContainer.dataset.key = '123asd123'
+    mapContainer.dataset.key = '7955ff0d-1f1e-4271-b357-70c071fadecb'
     document.body.append(mapContainer)
     window.map = createYMap(mapContainer, { setPlacemark: false, ui: false })
 })()
